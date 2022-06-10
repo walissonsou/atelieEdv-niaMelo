8f5ee363dec40990e3bd4305d77b4abc97cbb9ae
walissonsou <walissonsouza96@hotmail.com>
2022-06-10 14:09:43 -0300

    add NAvLink

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 import Home from '../src/component/Home/Home'
 import NavBar from '../src/component/NavBar/NavBar'
+import {Switch, Route} 'react-router-dom';
 function App() {
   return (
     <div className="App">
-       <NavBar /> 
-       <Home />
-      
+      <NavBar /> 
+        <Switch>
+          <Route exact path="/" component={Home}/>  
+          <Route exact path="/products" component={Products}/> 
+            <Home />
+        </Switch>
     </div>
   );
 }
